9b0343cae59c17fc413f59b216b39d762807447c
Anders Houghton <liland10@byu.edu>
2026-01-28 09:15:02 -0700

    Welcome message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1) The “Driver” class (the Program.cs class with the main method where the program begins) will:
+// • Welcome the user to the game
+// • Create a game board array to store the players’ choices
+// • Ask each player in turn for their choice and update the game board array
+// • Print the board by calling the method in the supporting class
+// 
+// • Check for a winner by calling the method in the supporting class,
+//checkWinner int[], printBoard int[] o is player 1, x is player 2
+// check for no
+// and notify the players when a win has occurred and which player won the game 
+
+using Section1Group9TicTac;
+
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
