238e5d56693f304d489c135579e59ebcceaac468
peter <peter@LAPTOP17>
2026-01-28 10:08:00 -0700

    oops

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿// Group 1-9 Jake Fuhriman, Anders Houghton, Merrick Morgan, Peter Young
+using Section1Group9TicTac;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+receiveBoard rb = new receiveBoard();
+string[] gameboard = ["x", "o", "x", "o", "x", "o", " ", "x", "o"];
+
+rb.printBoard(gameboard);
+
+string result = (rb.checkWinner(gameboard));
+if (result != "no")
+{
+    Console.WriteLine(result);
+}
+
+
+
